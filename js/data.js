a78538c78b20f16bde72fbd1c87c9c426251f137
LensXiong <lensxiong@gmail.com>
2017-09-18 14:20:50 +0800

    first commit

---
+++ js/data.js
@@ -0,0 +1,76 @@
+/* 
+* @Author: luuman
+* @Last Modified by:   luuman
+* @Date:   2016-03-09 13:13:56
+* @Last Modified time: 2016-04-07 14:29:18
+* @Function: Data FrontEndGuide
+    "Name":"name"
+    "Title":"title"
+    "Href":"htttp:"
+    "New":"new","New":"1"
+    {'Name':'','Title':'','Href':''}, 
+    {'Name':'','Title':'','Href':'',"New":"1"}, 
+*/
+var FrontGuide = {
+    "main":[{
+        "pageT1":"圈子",
+        "pageC1":[{
+            "pageT2":"前端教程",
+            "pageC2":[
+                {'Name':'慕课网','Title':'在线视频教程，在线代码练习','Href':'http://www.imooc.com/',"New":"1"}, 
+                {'Name':'极客学院','Title':'在线视频教学，强大的知识体系','Href':'http://www.jikexueyuan.com/'},
+                {'Name':'teamtree','Title':'改变你的事业。改变你的生活。','Href':'https://teamtreehouse.com/'},
+                {'Name':'Codecademy','Title':'Codecademy是最简单的方式来学习如何编码。这是互动的，有趣的，你可以做它与你的朋友','Href':'https://www.codecademy.com/learn',"New":"1"},
+                {'Name':'萌码','Title':'在线编程学习，实时问答，交互学习','Href':'http://www.mengma.com/'},
+                {'Name':'实验楼','Title':'第一家以实验为核心的IT在线教育平台','Href':'https://www.shiyanlou.com/'},
+                {'Name':'墨鱼前端','Title':'前端开发培训的公司','Href':'http://moyu-edu.com/'},
+            ]
+            },{
+            "pageT2":"前端知识",
+            "pageC2":[
+                {'Name':'w3cschool','Title':'万维网联盟创建于1994年，是WEB技术领域最具权威和影响力的国际中立性技术标准机构','Href':'http://www.w3school.com.cn/'},
+                {'Name':'菜鸟教程','Title':'学的不仅是技术，更是梦想！','Href':'http://www.runoob.com/'},
+                {'Name':'前端江湖','Title':'','Href':'http://www.qdjhu.com/sitemap3.html'},
+                {'Name':'Mozilla','Title':'Mozilla 开发者网络','Href':'https://developer.mozilla.org/zh-CN/'},
+                {'Name':'W3Help','Title':'系统的介绍某些知识点，帮助开发者正确理解该知识点的概念及与其他知识点的关联性，梳理知识体系。','Href':'http://www.w3help.org/zh-cn/kb/'},
+                {'Name':'看云','Title':'','Href':'http://www.kancloud.cn/'},
+                {'Name':'JS前端开发群月报','Title':'平日学习接触过的网站积累，以每月的形式发放','Href':'http://www.kancloud.cn/jsfront/month/82796/'},
+                {'Name':'前端开发规范手册','Title':'此手册主要实现的目标：代码一致性和最佳实践。通过代码风格的一致性，降低维护代码的成本以及改善多人协作的效率。同时遵守最佳实践，确保页面性能得到最佳优化和高效的代码。','Href':'http://zhibimo.com/read/Ashu/front-end-style-guide/index.html'},
+                {'Name':'Developer进阶书单','Title':'我深知自己的知识有限，所以写下本文以便和大家切磋交流。欢迎通过 GitHub 的Issues或者直接Pull Requests方式来分享你的经验。','Href':'http://phodal.github.io/booktree/#frontend'},
+                {'Name':'设计派导航','Title':'','Href':'http://hao.shejipai.cn/'},
+                {'Name':'前端人的俱乐部','Title':'做专业的前端平台，提供你需要的东西解放你的收藏夹，让它们只做最主要的事情','Href':'http://f2er.club/'},
+                {'Name':'觉唯','Title':'','Href':'http://www.jiawin.com/'},
+            ]
+        }]
+    },{
+        "pageT1":"圈子圈子",
+        "pageC1":[{
+            "pageT2":"前端教程",
+            "pageC2":[
+                {'Name':'慕课网','Title':'在线视频教程，在线代码练习','Href':'http://www.imooc.com/',"New":"1"}, 
+                {'Name':'极客学院','Title':'在线视频教学，强大的知识体系','Href':'http://www.jikexueyuan.com/'},
+                {'Name':'teamtree','Title':'改变你的事业。改变你的生活。','Href':'https://teamtreehouse.com/'},
+                {'Name':'Codecademy','Title':'Codecademy是最简单的方式来学习如何编码。这是互动的，有趣的，你可以做它与你的朋友','Href':'https://www.codecademy.com/learn',"New":"1"},
+                {'Name':'萌码','Title':'在线编程学习，实时问答，交互学习','Href':'http://www.mengma.com/'},
+                {'Name':'实验楼','Title':'第一家以实验为核心的IT在线教育平台','Href':'https://www.shiyanlou.com/'},
+                {'Name':'墨鱼前端','Title':'前端开发培训的公司','Href':'http://moyu-edu.com/'},
+            ]
+            },{
+            "pageT2":"前端知识",
+            "pageC2":[
+                {'Name':'w3cschool','Title':'万维网联盟创建于1994年，是WEB技术领域最具权威和影响力的国际中立性技术标准机构','Href':'http://www.w3school.com.cn/'},
+                {'Name':'菜鸟教程','Title':'学的不仅是技术，更是梦想！','Href':'http://www.runoob.com/'},
+                {'Name':'前端江湖','Title':'','Href':'http://www.qdjhu.com/sitemap3.html'},
+                {'Name':'Mozilla','Title':'Mozilla 开发者网络','Href':'https://developer.mozilla.org/zh-CN/'},
+                {'Name':'W3Help','Title':'系统的介绍某些知识点，帮助开发者正确理解该知识点的概念及与其他知识点的关联性，梳理知识体系。','Href':'http://www.w3help.org/zh-cn/kb/'},
+                {'Name':'看云','Title':'','Href':'http://www.kancloud.cn/'},
+                {'Name':'JS前端开发群月报','Title':'平日学习接触过的网站积累，以每月的形式发放','Href':'http://www.kancloud.cn/jsfront/month/82796/'},
+                {'Name':'前端开发规范手册','Title':'此手册主要实现的目标：代码一致性和最佳实践。通过代码风格的一致性，降低维护代码的成本以及改善多人协作的效率。同时遵守最佳实践，确保页面性能得到最佳优化和高效的代码。','Href':'http://zhibimo.com/read/Ashu/front-end-style-guide/index.html'},
+                {'Name':'Developer进阶书单','Title':'我深知自己的知识有限，所以写下本文以便和大家切磋交流。欢迎通过 GitHub 的Issues或者直接Pull Requests方式来分享你的经验。','Href':'http://phodal.github.io/booktree/#frontend'},
+                {'Name':'设计派导航','Title':'','Href':'http://hao.shejipai.cn/'},
+                {'Name':'前端人的俱乐部','Title':'做专业的前端平台，提供你需要的东西解放你的收藏夹，让它们只做最主要的事情','Href':'http://f2er.club/'},
+                {'Name':'觉唯','Title':'','Href':'http://www.jiawin.com/'},
+            ]
+        }]
+    }],
+}
